e9a7b98b6d2e6342dbc1bb78b7cd6e8f0d3cccbc
Andreas Franzén <andreas@devil.se>
2021-01-10 17:51:17 +0100

    use configs

--- run.py
+++ run.py
@@ -1,7 +1,42 @@
 import os
+import multiprocessing
 
-layers = ["128", "256", "512", "128 128", "256 256", "1024"]
+def work(config):
+    print(config)
+    return os.system(f'cargo run --release -- --stats-file {config["name"]}.csv --actor-layers {config["layer"]} --critic-layers {config["layer"]} --gamma {config["gamma"]} --max-episodes 30000 --render false > /dev/null 2>&1')
 
-for layer in layers:
-    print(layer)
-    os.system(f'cargo run --release -- --stats-file {layer.replace(" ","-")}.csv --actor-layers {layer} --critic-layers {layer} --max-episodes 20000')
+configs = [
+    {
+        "name": "l64-g999",
+        "layer": "64",
+        "gamma": "0.999",
+     },
+    {
+        "name": "l64-64-g999",
+        "layer": "64 64",
+        "gamma": "0.999",
+    },
+#    {
+#        "name": "l256-g999",
+#        "layer": "256",
+#        "gamma": "0.999",
+#    },
+#    {
+#        "name": "l256-256-g999",
+#        "layer": "256 256",
+#        "gamma": "0.999",
+#    },
+#    {
+#        "name": "l128-g999",
+#        "layer": "128",
+#        "gamma": "0.999",
+#    },
+#    {
+#        "name": "l128-128-g999",
+#        "layer": "128 128",
+#        "gamma": "0.999",
+#    },
+]
+
+with multiprocessing.Pool(2) as pool:
+    pool.map(work, configs)
