86505e4711ad22431badbbac0ec65c20659ab425
Andreas Franzén <andreas@devil.se>
2021-01-12 20:09:49 +0100

    ..

--- run.py
+++ run.py
@@ -3,47 +3,107 @@ import multiprocessing
 
 def work(config):
     print(config)
-    return os.system(f'cargo run --release -- --stats-file {config["name"]}g999.csv --actor-layers {config["layer"]} --gamma 0.999 --max-episodes 10000 --agent-max-age --render false > /dev/null 2>&1')
+    return os.system(f'cargo run --release -- --stats-file {config["name"]}g999.csv --actor-layers {config["layer"]} --model-type {config["model-type"]} --max-episodes 10000 --agent-max-age --render false > /dev/null 2>&1')
 
 configs = [
     {
-        "name": "l64",
+        "name": "l64a2c",
+        "model-type": "a2c",
         "layer": "64",
     },
     {
-        "name": "l64-64",
+        "name": "l64-64a2c",
+        "model-type": "a2c",
         "layer": "64 64",
     },
     {
-        "name": "l128",
+        "name": "l128a2c",
+        "model-type": "a2c",
         "layer": "128",
     },
     {
-        "name": "l128-128",
+        "name": "l128-128a2c",
+        "model-type": "a2c",
         "layer": "128 128",
     },
     {
-        "name": "l256",
+        "name": "l256a2c",
+        "model-type": "a2c",
         "layer": "256",
     },
     {
-        "name": "l256-256",
+        "name": "l256-256a2c",
+        "model-type": "a2c",
         "layer": "256 256",
     },
     {
-        "name": "l512",
+        "name": "l512a2c",
+        "model-type": "a2c",
         "layer": "512",
     },
     {
-        "name": "l512-512",
+        "name": "l512-512a2c",
+        "model-type": "a2c",
         "layer": "512 512",
     },
     {
-        "name": "l1024",
+        "name": "l1024a2c",
+        "model-type": "a2c",
         "layer": "1024",
     },
     {
-        "name": "l1024-1024",
+        "name": "l1024-1024a2c",
+        "model-type": "a2c",
+        "layer": "1024 1024",
+    },
+    {
+        "name": "l64ddqn",
+        "model-type": "ddqn",
+        "layer": "64",
+    },
+    {
+        "name": "l64-64ddqn",
+        "model-type": "ddqn",
+        "layer": "64 64",
+    },
+    {
+        "name": "l128ddqn",
+        "model-type": "ddqn",
+        "layer": "128",
+    },
+    {
+        "name": "l128-128ddqn",
+        "model-type": "ddqn",
+        "layer": "128 128",
+    },
+    {
+        "name": "l256ddqn",
+        "model-type": "ddqn",
+        "layer": "256",
+    },
+    {
+        "name": "l256-256ddqn",
+        "model-type": "ddqn",
+        "layer": "256 256",
+    },
+    {
+        "name": "l512ddqn",
+        "model-type": "ddqn",
+        "layer": "512",
+    },
+    {
+        "name": "l512-512ddqn",
+        "model-type": "ddqn",
+        "layer": "512 512",
+    },
+    {
+        "name": "l1024ddqn",
+        "model-type": "ddqn",
+        "layer": "1024",
+    },
+    {
+        "name": "l1024-1024ddqn",
+        "model-type": "ddqn",
         "layer": "1024 1024",
     },
 ]
