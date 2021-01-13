6dc53a79cf18ad873ae68ae0c7dd0f682a76c633
Andreas Franzén <andreas@devil.se>
2021-01-13 09:47:46 +0100

    ..

--- run.py
+++ run.py
@@ -1,112 +1,24 @@
 import os
 import multiprocessing
 
+
 def work(config):
     print(config)
-    return os.system(f'cargo run --release -- --stats-file {config["name"]}.csv --actor-layers {config["layer"]} --model-type {config["model-type"]} --max-episodes 10000 --agent-max-age 1000 --render false > /dev/null 2>&1')
+    return os.system(
+        f'cargo run --release -- --stats-file {config["model-type"]}-{config["layer"].replace(" ", "-")}.csv --actor-layers {config["layer"]} --model-type {config["model-type"]} --max-episodes 10000 --agent-max-age 1000 --render false > /dev/null 2>&1')
 
-configs = [
-    {
-        "name": "l64a2c",
-        "model-type": "a2c",
-        "layer": "64",
-    },
-    {
-        "name": "l64-64a2c",
-        "model-type": "a2c",
-        "layer": "64 64",
-    },
-    {
-        "name": "l128a2c",
-        "model-type": "a2c",
-        "layer": "128",
-    },
-    {
-        "name": "l128-128a2c",
-        "model-type": "a2c",
-        "layer": "128 128",
-    },
-    {
-        "name": "l256a2c",
-        "model-type": "a2c",
-        "layer": "256",
-    },
-    {
-        "name": "l256-256a2c",
-        "model-type": "a2c",
-        "layer": "256 256",
-    },
-    {
-        "name": "l512a2c",
-        "model-type": "a2c",
-        "layer": "512",
-    },
-    {
-        "name": "l512-512a2c",
-        "model-type": "a2c",
-        "layer": "512 512",
-    },
-    {
-        "name": "l1024a2c",
-        "model-type": "a2c",
-        "layer": "1024",
-    },
-    {
-        "name": "l1024-1024a2c",
-        "model-type": "a2c",
-        "layer": "1024 1024",
-    },
-    {
-        "name": "l64ddqn",
-        "model-type": "ddqn",
-        "layer": "64",
-    },
-    {
-        "name": "l64-64ddqn",
-        "model-type": "ddqn",
-        "layer": "64 64",
-    },
-    {
-        "name": "l128ddqn",
-        "model-type": "ddqn",
-        "layer": "128",
-    },
-    {
-        "name": "l128-128ddqn",
-        "model-type": "ddqn",
-        "layer": "128 128",
-    },
-    {
-        "name": "l256ddqn",
-        "model-type": "ddqn",
-        "layer": "256",
-    },
-    {
-        "name": "l256-256ddqn",
-        "model-type": "ddqn",
-        "layer": "256 256",
-    },
-    {
-        "name": "l512ddqn",
-        "model-type": "ddqn",
-        "layer": "512",
-    },
-    {
-        "name": "l512-512ddqn",
-        "model-type": "ddqn",
-        "layer": "512 512",
-    },
-    {
-        "name": "l1024ddqn",
-        "model-type": "ddqn",
-        "layer": "1024",
-    },
-    {
-        "name": "l1024-1024ddqn",
-        "model-type": "ddqn",
-        "layer": "1024 1024",
-    },
-]
+
+layers = ["64", "64 64", "512", "512 512", "1024", "1024 1024"]
+models = ["a2c", "ddqn"]
+
+configs = []
+
+for layer in layers:
+    for model in models:
+        configs.push({
+            "layer": layer,
+            "model-type": model,
+        })
 
 with multiprocessing.Pool(4) as pool:
     pool.map(work, configs)
