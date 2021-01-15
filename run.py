3c7f230cde444d7aa52d5d60215b19955a1f15e1
Andreas Franzén <andreas@devil.se>
2021-01-15 08:47:13 +0100

    ..

--- run.py
+++ run.py
@@ -5,12 +5,12 @@ import multiprocessing
 def work(config):
     print(config)
     return os.system(
-        f'cargo run --release -- --stats-file {config["model-type"]}-{config["layer"].replace(" ", "-")}-pm{config["prioritized-memory"]}-rpb-250k.csv --prioritized-memory {config["prioritized-memory"]} --actor-layers {config["layer"]} --model-type {config["model-type"]} --load-replay-buffer replay-buffer-250k.json --max-episodes 10000 --agent-max-age 1000 --render false > /dev/null 2>&1')
+        f'cargo run --release -- --stats-file {config["model-type"]}-{config["layer"].replace(" ", "-")}-pm{config["prioritized-memory"]}-rpb-1m.csv --prioritized-memory {config["prioritized-memory"]} --actor-layers {config["layer"]} --model-type {config["model-type"]} --load-replay-buffer replay-buffer-1m.json --max-episodes 30000 --agent-max-age 1000 --render false > /dev/null 2>&1')
 
 
-layers = ["512 512", "1024 1024"]
+layers = ["1024 1024 1024", "1024 1024"]
 models = ["a2c", "ddqn"]
-prioritized_memory = ["true", "false"]
+prioritized_memory = ["true"]  # , "false"]
 
 configs = []
 
@@ -20,7 +20,7 @@ for layer in layers:
             configs.append({
                 "layer": layer,
                 "model-type": model,
-                "prioritized-memory": pm,
+                "prioritized-memory": "true" if model == "ddqn" else "false",
             })
 
 with multiprocessing.Pool(4) as pool:
--- sandbox/bin/ml.rs
+++ sandbox/bin/ml.rs
@@ -409,8 +409,8 @@ impl Model for Model_a2c {
             .critic
             .forward(&states, &self.actor.forward(&states))
             .mean(Float);
-        let nd: ndarray::ArrayD<f64> = (&actor_loss.copy()).try_into().unwrap();
-        dbg!(&nd);
+        //let nd: ndarray::ArrayD<f64> = (&actor_loss.copy()).try_into().unwrap();
+        //dbg!(&nd);
         self.actor.optimizer.zero_grad();
         actor_loss.backward();
         self.actor.optimizer.step();
